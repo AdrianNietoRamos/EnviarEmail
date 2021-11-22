be6eda996a4ce12a7fa4fe3caab2e8527ea152e7
AdrianNietoRamos <adrianietoemfb@gmail.com>
2021-11-22 11:22:28 +0000

    Update Controller.java

--- src/main/java/enviaremail/Controller.java
+++ src/main/java/enviaremail/Controller.java
@@ -4,18 +4,29 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
+import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.util.converter.NumberStringConverter;
 
 public class Controller implements Initializable{
+	private Model model=new Model();
 	public Controller() throws IOException {
 		FXMLLoader loader=new FXMLLoader(getClass().getResource("/fxml/View.fxml"));
 		loader.setController(this);
@@ -60,22 +71,66 @@ public class Controller implements Initializable{
 
 	    @FXML
 	    void onCerrarButton(ActionEvent event) {
-
+Platform.exit();
 	    }
 
 	    @FXML
-	    void onEnviarButton(ActionEvent event) {
-
+	    void onEnviarButton(ActionEvent event)throws EmailException {
+	    	int puerto=model.getPuerto();
+	    	Alert enviado = new Alert(AlertType.CONFIRMATION);
+	    	Alert noEnviado = new Alert(AlertType.ERROR);
+	    	
+try {
+	Email email = new SimpleEmail();
+	email.setHostName(model.getServidor());
+	email.setSmtpPort(puerto);
+	email.setAuthenticator(new DefaultAuthenticator(model.getRemitente(), model.getPassword()));
+	email.setSSLOnConnect(model.isSsl());
+	email.setFrom(model.getRemitente());
+	email.setSubject(model.getAsunto());
+	email.setMsg(model.getMensaje());
+	email.addTo(model.getDestinatario());
+	email.send();
+	
+	enviado.setTitle("Mensaje enviado");
+	enviado.setContentText("Mensaje enviado a "+model.getDestinatario());
+	enviado.showAndWait();
+	
+	destinatarioText.clear();
+	asuntoText.clear();
+	mensajeText.clear();
+} catch (Exception e) {
+	noEnviado.setTitle("Error");
+	noEnviado.setContentText(e.getMessage());
+	noEnviado.setHeaderText("Error al enviar el mensaje");
+	noEnviado.showAndWait();
+}
 	    }
 
 	    @FXML
 	    void onLimpiarButton(ActionEvent event) {
-
+	    	
+	    	servidorText.clear();
+	    	puertoText.clear();
+	    	sslCheck.setSelected(false);
+	    	remitenteText.clear();
+	    	contraText.clear();
+	    	destinatarioText.clear();
+	    	asuntoText.clear();
+	    	mensajeText.clear();
 	    }
 
 
 public void initialize(URL location, ResourceBundle resources) {
-	// TODO Auto-generated method stub
+	Bindings.bindBidirectional(puertoText.textProperty(),model.puertoProperty(), new NumberStringConverter());
+	
+	model.servidorProperty().bindBidirectional(servidorText.textProperty());
+	model.sslProperty().bindBidirectional(sslCheck.selectedProperty());
+	model.remitenteProperty().bindBidirectional(remitenteText.textProperty());
+	model.passwordProperty().bindBidirectional(contraText.textProperty());
+	model.destinatarioProperty().bindBidirectional(destinatarioText.textProperty());
+	model.asuntoProperty().bindBidirectional(asuntoText.textProperty());
+	model.mensajeProperty().bindBidirectional(mensajeText.textProperty());
 	
 }
 public GridPane getView() {
